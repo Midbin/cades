1be24a92a05d63499b55c3ad7056fdc6530b597c
Niklas Ausborn <niklas@ausborn.de>
2023-11-10 11:12:59 +0100

    Added third party license notice

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -exuo pipefail
 yarn build
 cp dist/qrcode.iife.js typst-package/qrcode.js
+cp dist/third-party-licenses.txt typst-package/NOTICE
+echo "Dependencies:" > typst-package/NOTICE
+echo >> typst-package/NOTICE
+cat dist/third-party-licenses.txt >> typst-package/NOTICE
 cp README.md typst-package/
 cp LICENSE typst-package/
